f3ea0823dbaa18cc1593cbb550d59b8f4ab2b924
Miki Geoghegan <michaelageoghegan@hotmail.co.uk>
2021-06-22 13:07:51 +0200

    latest

--- 01_code-Express_intro/app.js
+++ 01_code-Express_intro/app.js
@@ -3,9 +3,39 @@
  * 
  * 1. mpm init -> initialize your node environment and packager
  * 2. npm install express
- * 3. Import the newly installed express module (library)
- * 4. Setup the express.app()
- * 5. Tell express.app() what it need to respond to (set a route)
- * 6. Prepare the .html ang .img resourses
- * 7. Sart the express.app() so it "listens" to a port
+ 
+ 
+
+
+ *7. Prepare the .html ang .img resourses
+ * 8. Route the files
  */
+
+//3. Import the newly installed express module (library)
+const express = require('express') // 'requre' built in function for node, telling application we want to use (import) the library in node_modules
+
+//4. Setup the express.app()
+const app = express() 
+
+//5. Tell express.app() what it needs to respond to (set a URL route) - URL is the address on the envelop, the route is the URL plus the action you want to take on that URL
+
+app.all('/', mainRouteHandler)
+
+
+function mainRouteHandler(request, response) {
+    // just inspect the http verb in the console
+    console.log(request.method)
+    // return some text to the user
+    response.send(`<h1>Hey you send me a request ${request.method}</h1>`)
+}
+
+app.all('/hello', helloRouteHandler)
+
+function helloRouteHandler(request, response) {
+    response.send(`<h1>Hello from my first Express.js app!</h1>`)
+}
+
+// 6. Sart the express.app() so it "listens" to a port
+
+app.listen(3000)
+
